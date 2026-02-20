14f6caa2f5133b9b04a4e251834856db8424fcf0
Bennett Bullock <bennettbullock@Bennetts-MacBook-Pro.local>
2026-02-19 20:20:47 -0500

    Documented it

--- similarity.py
+++ similarity.py
@@ -13,7 +13,9 @@ ENCODER=tiktoken.get_encoding("cl100k_base")
 def vectors_and_lengths(strings:List[str],
                         encoder:tiktoken.core.Encoding=ENCODER,
                        ) -> Tuple[np.array,np.array]:
-
+    '''
+    Helper to extract embeddings and log token counts for a list of strings.
+    '''
     vecs=get_embeddings(strings)
     lengths=np.array([np.log(len(encoder.encode(st))) for st in strings])
 
@@ -24,7 +26,13 @@ def strong_recommendations(ranking:np.array,
                            similarities:np.array,
                            diffs:np.array,
                            strings:List[str]) -> Set[str]:
+    '''
+    Function to gather a set of strong recommendations for string inclusion.
 
+    1) Test if the rank is above 1.
+    2) If it is, take two intervals, d0=|sim - 1/delta| and d1=|1-sim|.
+    3) If d0 > d1, include in recommendations.
+    '''
     recommendations={}
 
     for (rank,similarity,diff,st) in zip(ranking,similarities,diffs,strings):
@@ -47,36 +55,49 @@ def submit_query(q:str,
                  lengths:np.array=None,
                  precomputedEmbeddings:np.array=None,
                 ) -> List[Dict]:
+    '''
+    Main function to run queries.  
 
-    if precomputedEmbeddings is None or lengths is None:
+    1) If lengths and precomputedEmbeddings are not None, they will be used as precomputed 
+    lengths and embeddings.  Otherwise compute the embeddings.
+    2) Compute lengths and embeddings for query.
+    3) Compute the diffs, similarities, and rankings.
+    4) Compute recommendations.
+    5) Create a closure to return a string for each recommendation - 'probable', 'strongly
+    probable', or 'undetermined'.
+    6) Create a list of dicts, each dict containing the string, ranking, and recommendation.
+    Sort based on ranking.
+    '''
+    if precomputedEmbeddings is None or lengths is None: # (1)
 
         lengths,embeddings=vectors_and_lengths(strings)
 
-    queryLength,queryEmbedding=vectors_and_lengths([q])
+    queryLength,queryEmbedding=vectors_and_lengths([q]) # (2) 
     queryLength=queryLength[0]
     queryEmbedding=queryEmbedding[0]
     
-    diffs=lengths-queryLength
+    diffs=lengths-queryLength # (3)
     similarities=np.dot(embeddings,queryEmbedding)
     ranking=similarities*diffs
-    recommendations=strong_recommendations(ranking,similarities,diffs,strings)
 
-    def recommendation_f(st,rnk):
+    recommendations=strong_recommendations(ranking,similarities,diffs,strings) # (4)
+
+    def recommendation_f(st,rnk): # (5)
 
         if rnk > 1:
 
-            return 'probable string inclusion'
+            return 'probable'
 
         elif st in recommendations:
 
-            return 'strong probability of string inclusion'
+            return 'strongly probabile'
 
         else:
 
             return 'undetermined'
 
 
-    distancesAndStrings=[{'string':st,
+    distancesAndStrings=[{'string':st,  # (6)
                           'ranking':float(rnk),
                           'recommendation':recommendation_f(st,rnk),
                          } for (st,rnk) in zip(strings,ranking)]
